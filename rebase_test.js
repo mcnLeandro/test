d1113510a024598ba9e3248454fe5d1a623cc904
mcnLeandro <leandro83g.development@gmail.com>
2023-11-28 18:47:36 +0900

    rebaseのテスト修正

--- rebase_test.js
+++ rebase_test.js
@@ -1,32 +1,32 @@
 // NOTE: rebaseのテスト
 
 // テスト1
-// 1. rebase_test1_core: mainから作成
-// 2. rebase_test1_front: mainから作成
-// 3. rebase_test1_core: commit追加してpush(coreの作業として)
-// 4. rebase_test1_front: coreをpullしてpush
-// 5. rebase_test1_front: commitを追加してpush(frontの作業として)
-// 6. rebase_test1_core: mainにマージ
-// 7. rebase_test1_front: mainをpull --rebaseしてpush
+// 0. rebase_test1_core: mainから作成
+// 0. rebase_test1_front: mainから作成
+// 1. rebase_test1_core: commit追加してpush(coreの作業として)
+// 2. rebase_test1_front: coreをpullしてpush
+// 3. rebase_test1_front: commitを追加してpush(frontの作業として)
+// 4. rebase_test1_core: mainにマージ
+// 5. rebase_test1_front: mainをpull --rebaseしてpush
 // ここでrebase_test1_frontのPRがどうなっているか確認
-// 期待: 5番目のコミットのみが差分になってほしい
+// 期待: 3番目のコミットのみが差分になってほしい
 
 const work1 = () => {
 
 }
 
 // テスト2
-// 1. rebase_test1_core: mainから作成
-// 2. rebase_test1_front: mainから作成
-// 3. rebase_test2_core: commit追加してpush(coreの作業として)
+// 0. rebase_test1_core: mainから作成
+// 0. rebase_test1_front: mainから作成
+// 1. rebase_test2_core: commit追加してpush(coreの作業として)
+// 2. rebase_test2_front: coreをpullしてpush
+// 3. rebase_test2_core: commitを書き換えて-f push
 // 4. rebase_test2_front: coreをpullしてpush
-// 5. rebase_test2_core: commitを書き換えて-f push
-// 6. rebase_test2_front: coreをpullしてpush
-// 7. rebase_test2_front: commitを追加してpush(frontの作業として)
-// 8. rebase_test2_core: mainにマージ
-// 9. rebase_test2_front: mainをpull --rebaseしてpush
+// 5. rebase_test2_front: commitを追加してpush(frontの作業として)
+// 6. rebase_test2_core: mainにマージ
+// 7. rebase_test2_front: mainをpull --rebaseしてpush
 // ここでrebase_test2_frontのPRがどうなっているか確認
-// 期待: 7番目のコミットのみが差分になってほしい
+// 期待: 5番目のコミットのみが差分になってほしい
 
 const work2 = () => {
 
